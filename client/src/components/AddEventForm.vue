1a316b34eb78f1a6de3a150ba7726a0cadbc6649
fionwan <fion.wan8@gmail.com>
2018-06-26 16:05:17 -0600

    updated form

--- client/src/components/AddEventForm.vue
+++ client/src/components/AddEventForm.vue
@@ -1,35 +1,68 @@
 <template>
-    <v-form v-model="valid">
-        <v-text-field
-        v-model="name"
-        :rules="nameRules"
-        :counter="10"
-        label="Name"
-        required
-        ></v-text-field>
-        <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-        ></v-text-field>
-    </v-form>
+    <v-layout row justify-center>
+        <v-dialog v-model="dialog" persistent max-width="500px">
+        <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+        <v-card>
+            <v-card-title>
+            <span class="headline">User Profile</span>
+            </v-card-title>
+            <v-card-text>
+            <v-container grid-list-md>
+                <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                    <v-text-field label="Legal first name" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                    <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                    <v-text-field
+                    label="Legal last name"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                    ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                    <v-text-field label="Email" required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                    <v-text-field label="Password" type="password" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6>
+                    <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age"
+                    required
+                    ></v-select>
+                </v-flex>
+                <v-flex xs12 sm6>
+                    <v-select
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                    autocomplete
+                    chips
+                    ></v-select>
+                </v-flex>
+                </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+            </v-card-actions>
+        </v-card>
+        </v-dialog>
+    </v-layout>
 </template>
 
 <script>
     export default {
         data: () => ({
-        valid: false,
-        name: '',
-        nameRules: [
-            v => !!v || 'Name is required',
-            v => v.length <= 10 || 'Name must be less than 10 characters'
-        ],
-        email: '',
-        emailRules: [
-            v => !!v || 'E-mail is required',
-            v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-        ]
+        dialog: false
         })
     }
 </script>
