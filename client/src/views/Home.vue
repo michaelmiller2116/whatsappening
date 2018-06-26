433b08727fa0a31373e34ab16eeac74865eb124f
fionwan <fion.wan8@gmail.com>
2018-06-26 11:54:17 -0600

    added event cards

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
+  <div>
   <div class="home">
     <Map />
+  </div>
+  <div>
+    <PreviewCards />
+  </div>
   </div>
 </template>
 
 <script>
 
 import Map from '@/components/Map';
+import PreviewCards from '@/components/PreviewCards';
 
 export default {
   name: 'home',
   components: {
     Map,
-    
+    PreviewCards,
   }
 }
 </script>
