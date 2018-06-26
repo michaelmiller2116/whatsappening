753a323133888dd0595bdebf2a72b9828fabd782
michaelmiller2116 <michael.miller2116@gmail.com>
2018-06-26 15:57:21 -0600

    settled merge conflict in app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
     <Header />
-<<<<<<< Updated upstream
     <router-view :eventData="eventArr"/>
-=======
-      <div id="button-container" class="center">
-        <a v-on:click="getEvents()" class="waves-effect waves-light orange darken-2 btn">Food &amp; Drink</a>
-        <a v-on:click="getEvents()" class="waves-effect waves-light green darken-1 btn">Active</a>
-      </div>
-    <router-view/>
->>>>>>> Stashed changes
   </div>
 </template>
 
@@ -21,7 +13,6 @@ export default {
   components: {
     Header,
   },
-<<<<<<< Updated upstream
   data: () => ({
     eventArr: [],
   }),
@@ -29,19 +20,6 @@ export default {
     this.eventArr = await API.getEvents();
   },
 };
-=======
-
-  methods: {
-    getEvents() {
-      fetch('https://whatsappening-server.herokuapp.com/events')
-        .then(response => response.json())
-        .then(response => console.log(response))
-    }
-  }
-}
-
-
->>>>>>> Stashed changes
 </script>
 
 <style scoped>
