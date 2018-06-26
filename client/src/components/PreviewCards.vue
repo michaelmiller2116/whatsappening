9b4911a17708edb4b288890e7c5b7f32d856e9ce
Travis Roach <travis.roach24@gmail.com>
2018-06-26 15:01:10 -0600

    display event modal on click

--- client/src/components/PreviewCards.vue
+++ client/src/components/PreviewCards.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="cards">
-        <div id="preview-card" class="col s12 m7">
+    <div>
+        <div :ref="event" id="preview-card" class="col s12 m7">
             <div class="card horizontal">
             <div class="card-image">
                 <img :src="event.imageURL">
@@ -12,18 +12,56 @@
                 <p>{{event.date}}</p>
                 </div>
                 <div class="card-action">
-                <a href="#">Details</a>
+                <v-btn color="primary" dark @click.stop="dialog3 = true, getEventById()">Details</v-btn>
                 </div>
             </div>
             </div>
         </div>
+        <v-layout row justify-center>
+            <v-dialog v-model="dialog3" max-width="60%">
+            <v-card>
+                <v-card-title class="card-content">
+                <div class="image-container">
+                    <img class="modal-image" :src="selectedEvent.imageURL">
+                </div>
+                <h1>{{ selectedEvent.title }}</h1>
+                <strong>{{ selectedEvent.date }}</strong>
+                <strong>{{ selectedEvent.location }}</strong>
+                <v-spacer></v-spacer>
+                <p>Here</p>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <strong>{{ selectedEvent.description }}</strong>
+                <p>Contact Establishment: {{ selectedEvent.email }}</p>
+                <v-spacer></v-spacer>
+                </v-card-title>
+                <v-card-actions>
+                <v-btn color="black" flat @click.stop="dialog3=false">Close</v-btn>
+                </v-card-actions>
+            </v-card>
+            </v-dialog>
+        </v-layout>
     </div>
+
 </template>
 
 <script>
-    export default {
-        props: ["event"],
-    };
+import API from '@/API.js';
+
+export default {
+    props: ["event"],
+    data () {
+        return {
+            dialog3: false,
+            selectedEvent: Object,
+        }
+    },
+    methods: {
+        getEventById() {
+            this.selectedEvent = this.event
+        }
+    },
+};
     
 </script>
 
@@ -32,11 +70,28 @@
     #preview-card {
         width: 350px;
         height: 150px;
+        margin: 20px;
     }
 
     #preview-card img {
         width: 150px;
         height: 165px;
+    }
+
+    .image-container {
+        text-align: center;
+        width: 100%;
+    }
+
+    .card-content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start ! important;
+    }
+
+    .modal-image {
+        width: 500px;
+        height: 300px;
     }
 
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,34 +1,7 @@
 <template>
   <div>
   <div class="home">
-    <div>
-    <v-layout row justify-center>
-      
-      <v-btn color="primary" dark @click.stop="dialog3 = true">Open Dialog 3</v-btn>
-      
-      <v-dialog v-model="dialog3" max-width="60%">
-        <v-card>
-          <v-card-title class="card-content">
-            <div class="image-container">
-              <img src="http://via.placeholder.com/500x300">
-            </div>
-            <h1>Title</h1>
-            <strong>Location:</strong>
-            <v-spacer></v-spacer>
-            <p>Here is the location</p>
-            <v-spacer></v-spacer>
-            <v-spacer></v-spacer>
-            <strong>Description:</strong>
-            <v-spacer></v-spacer>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nulla cum ratione recusandae quae quam laborum molestiae autem fugiat provident totam praesentium dolorum commodi reiciendis possimus distinctio aspernatur deleniti voluptas magni, accusantium sint, dolor maxime sunt animi! Libero, ut eaque. Ex quo aliquid vel. Vero assumenda esse quasi temporibus a eos quos et, eligendi repellendus omnis quae fugiat earum aliquid ea blanditiis quibusdam quidem id sit fugit nulla! Tempora quaerat suscipit alias. Quae non numquam quasi adipisci modi, eaque aliquam deleniti id nobis exercitationem ratione iusto enim tempora corporis veniam, labore laboriosam nemo officiis consequuntur. Eligendi itaque temporibus alias sapiente?</p>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn color="black" flat @click.stop="dialog3=false">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-layout>
-  </div>
+    
     <Map />
   </div>
   <div class="cards">
@@ -69,10 +42,11 @@ export default {
 }
 
 .cards {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-      flex-wrap: wrap;
-  }
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  height: 1000px; 
+}
 </style>
 
