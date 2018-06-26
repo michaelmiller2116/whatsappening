1dd1f14cba215500127c4e032dc6fd14f26e52a5
fionwan <fion.wan8@gmail.com>
2018-06-26 10:32:58 -0600

    rendered google maps img

---
+++ client/src/components/Map.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Google Maps Image</h1>
+        <img src='../assets/gmaps-static.png'>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+</style>
