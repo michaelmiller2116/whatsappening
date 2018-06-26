af9685c61ae7e1b2772b616da9cc1c2079eda1f7
fionwan <fion.wan8@gmail.com>
2018-06-26 15:50:11 -0600

    branching to fion

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <Header />
+<<<<<<< Updated upstream
     <router-view :eventData="eventArr"/>
+=======
+      <div id="button-container" class="center">
+        <a v-on:click="getEvents()" class="waves-effect waves-light orange darken-2 btn">Food &amp; Drink</a>
+        <a v-on:click="getEvents()" class="waves-effect waves-light green darken-1 btn">Active</a>
+      </div>
+    <router-view/>
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -13,6 +21,7 @@ export default {
   components: {
     Header,
   },
+<<<<<<< Updated upstream
   data: () => ({
     eventArr: [],
   }),
@@ -20,8 +29,22 @@ export default {
     this.eventArr = await API.getEvents();
   },
 };
+=======
+
+  methods: {
+    getEvents() {
+      fetch('https://whatsappening-server.herokuapp.com/events')
+        .then(response => response.json())
+        .then(response => console.log(response))
+    }
+  }
+}
+
+
+>>>>>>> Stashed changes
 </script>
 
 <style scoped>
 
 </style>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,6 +3,9 @@
     <div class="map-container">
       <Map />
     </div>
+    <div>
+      <AddEventForm />
+    </div>
     <div class="cards-container">
       <h4>Events Near You</h4>
       <PreviewCards :event="event" :key="event.id" v-for="event in eventData"/>
@@ -13,12 +16,14 @@
 <script>
 import Map from '@/components/Map';
 import PreviewCards from '@/components/PreviewCards';
+import AddEventForm from '@/components/AddEventForm';
 
 export default {
   name: 'home',
   components: {
     Map,
     PreviewCards,
+    AddEventForm,
   },
   props: ['eventData'],
   data () {
