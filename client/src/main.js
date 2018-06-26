2daafa08ca17c8664b94c08ff0d3047e29416e35
Travis Roach <travis.roach24@gmail.com>
2018-06-26 10:23:39 -0600

    building base component structure

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/dist/vuetify.min.css'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
