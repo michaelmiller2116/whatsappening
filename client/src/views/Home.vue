5a89b33a5132e9af727c742d1fcc52c84810db5b
michaelmiller2116 <michael.miller2116@gmail.com>
2018-06-27 11:11:51 -0600

    enlarged map and added you are here in home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="main-container">
     <div class="map-container">
+      <h4>You are here</h4>
       <!-- <Map /> -->
       <GmapMap
         :center="{lat:39.7392, lng:-104.9903}"
-        :zoom="7"
+        :zoom="13"
         map-type-id="terrain"
-        style="width: 500px; height: 300px"
+        style="width: 70vw; height: 75vh"
       >
       <!-- <gmap-circle
         v-if="displayCircle"
